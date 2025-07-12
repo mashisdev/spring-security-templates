400b5f1dcec6bba8ee7b2d9342622279f6bb997d
Manuel Schaller <manuelschallerrosal@gmail.com>
2025-07-12 16:33:53 -0300

    1º commit of RBAC template

---
+++ roles/src/test/java/com/jwt/roles/RolesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.roles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
