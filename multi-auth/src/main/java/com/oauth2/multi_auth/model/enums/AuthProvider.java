048ebf5dcf8846dba61c834f3a7b77ea2e84fa2c
manuelsrl <manuelschallerrosal@gmail.com>
2025-07-25 11:43:10 -0300

    Facebook and LinkedIn providers added

--- multi-auth/src/main/java/com/oauth2/multi_auth/model/enums/AuthProvider.java
+++ multi-auth/src/main/java/com/oauth2/multi_auth/model/enums/AuthProvider.java
@@ -2,5 +2,7 @@ package com.oauth2.multi_auth.model.enums;
 
 public enum AuthProvider {
     google,
+    facebook,
     github,
+    linkedin,
 }
--- multi-auth/src/main/java/com/oauth2/multi_auth/security/oauth2/user/OAuth2UserInfoFactory.java
+++ multi-auth/src/main/java/com/oauth2/multi_auth/security/oauth2/user/OAuth2UserInfoFactory.java
@@ -10,8 +10,12 @@ public class OAuth2UserInfoFactory {
     public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
         if (registrationId.equalsIgnoreCase(AuthProvider.google.toString())) {
             return new GoogleOAuth2UserInfo(attributes);
+        } else if (registrationId.equalsIgnoreCase(AuthProvider.facebook.toString())) {
+            return new FacebookOAuth2UserInfo(attributes);
         } else if (registrationId.equalsIgnoreCase(AuthProvider.github.toString())) {
             return new GithubOAuth2UserInfo(attributes);
+        } else if (registrationId.equalsIgnoreCase(AuthProvider.linkedin.toString())) {
+            return new LinkedinOAuth2UserInfo(attributes);
         } else {
             throw new OAuth2AuthenticationProcessingException(String.format("Login with %s is not supported.", registrationId));
         }
