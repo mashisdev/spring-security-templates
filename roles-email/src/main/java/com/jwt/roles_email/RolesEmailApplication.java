85b5271a1c2a870d67d87aa35d53d9575e330c44
manuelsrl <manuelschallerrosal@gmail.com>
2025-07-25 19:20:55 -0300

    Roles and email verification project started

---
+++ roles-email/src/main/java/com/jwt/roles_email/RolesEmailApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.roles_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolesEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolesEmailApplication.class, args);
+	}
+
+}
---
+++ roles-email/src/test/java/com/jwt/roles_email/RolesEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.roles_email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolesEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
