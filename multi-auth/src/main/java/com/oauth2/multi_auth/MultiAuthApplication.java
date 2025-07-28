eff4949bf06525560d9598508e31ec77df0f0ebc
manuelsrl <manuelschallerrosal@gmail.com>
2025-07-28 13:28:28 -0300

    MySQL properties added and @EnableConfigurationProperties added to main Application class

--- multi-auth/src/main/java/com/oauth2/multi_auth/MultiAuthApplication.java
+++ multi-auth/src/main/java/com/oauth2/multi_auth/MultiAuthApplication.java
@@ -1,9 +1,12 @@
 package com.oauth2.multi_auth;
 
+import com.oauth2.multi_auth.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class MultiAuthApplication {
 
 	public static void main(String[] args) {
