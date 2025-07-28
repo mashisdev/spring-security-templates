673c042c170681374530faf94d76d36bb8b92b01
manuelsrl <manuelschallerrosal@gmail.com>
2025-07-28 15:03:32 -0300

    AuthenticationService register function completed

---
+++ roles-email/src/main/java/com/jwt/roles_email/auth/AuthenticationController.java
@@ -0,0 +1,51 @@
+package com.jwt.roles_email.auth;
+
+import com.jwt.roles_email.auth.request.AuthenticationRequest;
+import com.jwt.roles_email.auth.request.RegisterRequest;
+import com.jwt.roles_email.auth.response.AuthenticationResponse;
+import com.jwt.roles_email.user.dto.UserDto;
+import com.jwt.roles_email.user.entity.User;
+import com.jwt.roles_email.user.mapper.UserMapper;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+@Slf4j
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+    private final UserMapper userMapper;
+
+    @PostMapping("/register")
+//    @RateLimiter(name = "authRateLimiter")
+    public ResponseEntity<UserDto> register(@RequestBody @Valid RegisterRequest request) {
+        log.info("Received registration request for email: {}", request.email());
+
+        User user = userMapper.registerRequestToUser(request);
+        log.debug("Mapped RegisterRequest to User object for email: {}", user.getEmail());
+
+        UserDto response = authenticationService.register(user);
+        log.info("User registered successfully. Returning authentication token.");
+        return ResponseEntity.created(response);
+    }
+
+//    @PostMapping("/login")
+////    @RateLimiter(name = "authRateLimiter")
+//    public ResponseEntity<AuthenticationResponse> login(@RequestBody @Valid AuthenticationRequest request) {
+//        log.info("Received login request for email: {}", request.email());
+//
+//        AuthenticationResponse response = authenticationService.authenticate(request);
+//        log.info("User logged in successfully. Returning authentication token.");
+//        return ResponseEntity.ok(response);
+//    }
+
+}
--- roles-email/src/main/java/com/jwt/roles_email/auth/AuthenticationService.java
+++ roles-email/src/main/java/com/jwt/roles_email/auth/AuthenticationService.java
@@ -3,13 +3,16 @@ package com.jwt.roles_email.auth;
 import com.jwt.roles_email.auth.request.AuthenticationRequest;
 import com.jwt.roles_email.auth.request.RegisterRequest;
 import com.jwt.roles_email.auth.response.AuthenticationResponse;
+import com.jwt.roles_email.config.EmailService;
 import com.jwt.roles_email.config.JwtService;
 import com.jwt.roles_email.exception.user.UserAlreadyRegisteredException;
 import com.jwt.roles_email.exception.user.WrongEmailOrPasswordException;
+import com.jwt.roles_email.user.dto.UserDto;
 import com.jwt.roles_email.user.entity.Role;
 import com.jwt.roles_email.user.entity.User;
 import com.jwt.roles_email.user.mapper.UserMapper;
 import com.jwt.roles_email.user.repository.UserRepository;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,7 +20,9 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
@@ -27,14 +32,12 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final EmailService emailService;
     private final AuthenticationManager authenticationManager;
     private final UserMapper userMapper;
 
-    public AuthenticationResponse register(RegisterRequest request) {
-        log.info("Attempting to register new user with email: {}", request.email());
-
-        User user = userMapper.registerRequestToUser(request);
-        log.debug("Mapped RegisterRequest to User object for email: {}", user.getEmail());
+    public UserDto register(User user) {
+        log.info("Attempting to register new user with email: {}", user.getEmail());
 
         if (userRepository.findByEmail(user.getEmail()).isPresent()) {
             log.warn("Registration failed: User with email {} is already registered.", user.getEmail());
@@ -48,49 +51,81 @@ public class AuthenticationService {
         user.setRole(Role.USER);
         log.debug("Role assigned to user: {}", user.getEmail());
 
-        User saved = userRepository.save(user);
-        log.info("User {} registered successfully with ID: {}", saved.getEmail(), saved.getId());
+        user.setEnabled(false);
+        user.setVerificationCode(generateVerificationCode());
+        user.setVerificationCodeExpiresAt(LocalDateTime.now().plusMinutes(30));
+        log.debug("Verification code generated and set for user {}. Code expires at: {}", user.getEmail(), user.getVerificationCodeExpiresAt());
 
-        String jwtToken = jwtService.generateToken(userMapper.userToUserEntity(user));
-        log.debug("JWT token generated for registered user: {}", saved.getEmail());
+        log.debug("Attempting to send verification email to user: {}", user.getEmail());
+        sendVerificationEmail(user);
+        log.debug("Verification email sent successfully to user: {}", user.getEmail());
 
-        return AuthenticationResponse.builder()
-                .token(jwtToken)
-                .build();
+        userRepository.save(user);
+        log.info("User {} registered successfully with ID: {}", user.getEmail(), user.getId());
+
+        return userMapper.userToUserDto(user);
     }
 
-    public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        log.info("Attempting to authenticate user with email: {}", request.email());
+//    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+//        log.info("Attempting to authenticate user with email: {}", request.email());
+//
+//        Optional<User> userOptional = userRepository.findByEmail(request.email());
+//        if (userOptional.isEmpty()) {
+//            log.warn("Authentication failed: User with email {} not found.", request.email());
+//            throw new WrongEmailOrPasswordException("Wrong email or password");
+//        }
+//        User user = userOptional.get();
+//        log.debug("User found for email: {}", user.getEmail());
+//
+//        if (!passwordEncoder.matches(request.password(), user.getPassword())) {
+//            log.warn("Authentication failed: Password mismatch for user {}.", request.email());
+//            throw new WrongEmailOrPasswordException("Wrong email or password");
+//        }
+//        log.debug("Password matched for user: {}", user.getEmail());
+//
+//        authenticationManager.authenticate(
+//                new UsernamePasswordAuthenticationToken(
+//                        request.email(),
+//                        request.password()
+//                )
+//        );
+//        log.info("User {} authenticated successfully via AuthenticationManager.", request.email());
+//
+//        String jwtToken = jwtService.generateToken(userMapper.userToUserEntity(user));
+//        log.debug("JWT token generated for authenticated user: {}", user.getEmail());
+//
+//        return AuthenticationResponse.builder()
+//                .token(jwtToken)
+//                .build();
+//
+//    }
 
-        Optional<User> userOptional = userRepository.findByEmail(request.email());
-        if (userOptional.isEmpty()) {
-            log.warn("Authentication failed: User with email {} not found.", request.email());
-            throw new WrongEmailOrPasswordException("Wrong email or password");
+    private void sendVerificationEmail(User user) {
+        String subject = "Jwt Email Account Verification";
+        String verificationCode = "VERIFICATION CODE " + user.getVerificationCode();
+        String htmlMessage = "<html>"
+                + "<body style=\"font-family: Arial, sans-serif;\">"
+                + "<div style=\"background-color: #f5f5f5; padding: 20px;\">"
+                + "<h2 style=\"color: #333;\">Welcome to our app!</h2>"
+                + "<p style=\"font-size: 16px;\">Please enter the verification code below to continue:</p>"
+                + "<div style=\"background-color: #fff; padding: 20px; border-radius: 5px; box-shadow: 0 0 10px rgba(0,0,0,0.1);\">"
+                + "<h3 style=\"color: #333;\">Verification Code:</h3>"
+                + "<p style=\"font-size: 18px; font-weight: bold; color: #007bff;\">" + verificationCode + "</p>"
+                + "</div>"
+                + "</div>"
+                + "</body>"
+                + "</html>";
+
+        try {
+            emailService.sendVerificationEmail(user.getEmail(), subject, htmlMessage);
+        } catch (MessagingException e) {
+            e.printStackTrace();
         }
-        User user = userOptional.get();
-        log.debug("User found for email: {}", user.getEmail());
+    }
 
-        if (!passwordEncoder.matches(request.password(), user.getPassword())) {
-            log.warn("Authentication failed: Password mismatch for user {}.", request.email());
-            throw new WrongEmailOrPasswordException("Wrong email or password");
-        }
-        log.debug("Password matched for user: {}", user.getEmail());
-
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.email(),
-                        request.password()
-                )
-        );
-        log.info("User {} authenticated successfully via AuthenticationManager.", request.email());
-
-        String jwtToken = jwtService.generateToken(userMapper.userToUserEntity(user));
-        log.debug("JWT token generated for authenticated user: {}", user.getEmail());
-
-        return AuthenticationResponse.builder()
-                .token(jwtToken)
-                .build();
-
+    private Integer generateVerificationCode() {
+        Random random = new Random();
+        return random.nextInt(900000) + 100000;
     }
 }
 
